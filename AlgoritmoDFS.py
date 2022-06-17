4cbe8beef14cbed91129ba16c3146c70ddb9de54
LuisOlalla <75482079+LuisOlalla@users.noreply.github.com>
2022-06-16 20:35:36 -0500

    Nodos

--- AlgoritmoDFS.py
+++ AlgoritmoDFS.py
@@ -13,4 +13,32 @@ los nodos a considerar para obtener la búsqueda deseada
 class Grafo():
     
     def __init__(self, numero_de_nodos,dirigido=True):
+        '''
+        Generar el constructor, este método permite instanciar el objeto creado
+            Parámetros:
+                numero_de_nodos : int
+                    Determina el rango que tendra el grafo
+                
+                dirigido : boolean
+                    Determina si el grafo es dirigido o no
+            Retorna:
+                Nada
+        Generar un control de excepciones
+         try:
+            except:
+                print("Error")
+        '''
+        try:
+            #Asignamos valores al número de nodos mediante el parámetro que se recibio
+            self.m_numero_nodos = numero_de_nodos
+            #Se genera el rango de nodos en base a m_numero_nodos
+            self.m_nodos = range(self.m_numero_nodos)
+            #Determinar si el grafo es dirigido o no dirigido
+            self.m_dirigido = dirigido
+            #Se genera un diccionario con los nodos que se van a visitar
+        
+            #Se genera una lista de los nodos que se van a visitar
+            self.m_lista_adyacencia = {nodo: set() for nodo in self.m_nodos}
+        except Exception as e:
+            print(e)
         
